559da2e03b91e9161025a9f1f0a87ef9763f3255
fleurywanttosleep <calaakarina@gmail.com>
2025-05-28 00:19:33 +0300

    Вывод цифр

--- src/MyPanel.java
+++ src/MyPanel.java
@@ -16,36 +16,56 @@ public class MyPanel extends JPanel {
 
     private int y = 100;
     
-    private int width = 200;
+    private int width = 50;
 
-    private int height = 200;
+    private int height = 100;
+    
+    private int step = 30;
 
-    private int delta = 10;
+    private int delta = 5;
 
     private ArrayList<Rectangle> outlList = new ArrayList<Rectangle>();
 
     
     public void setSymbols(String parm) {
-        segment(1);
-        segment(2);
-        segment(3);
-        segment(4);
-        segment(5);
-        segment(6);
-        segment(7);
+        String[] arr = parm.replace(" ", " " ).split(" ");
+        for (String cifra:arr) {
+            switch (cifra) {
+                case "1":
+                    segment(2);
+                    segment(4);
+                    break;
+                case "2":
+                    segment(1);
+                    segment(2);
+                    segment(3);
+                    segment(6);
+                    segment(5);
+                    break;
+                case "3":
+                    segment(1);
+                    segment(2);
+                    segment(3);
+                    segment(4);
+                    segment(5);
+                    break;
+            }
+            x = x + step + width;
+        }
+        
     }
 
     private void segment(int number) {
         switch (number) {
-            case 1:outlList.add(new Rectangle(x+delta, y, width+delta, y));
+            case 1:outlList.add(new Rectangle(x+delta, y, x+width-delta, y));
                 break;
-            case 2:outlList.add(new Rectangle(width, y+delta, width, y+(height/2)-delta));
+            case 2:outlList.add(new Rectangle(x+width, y+delta, x+width, y+(height/2)-delta));
                 break;
-            case 3:outlList.add(new Rectangle(x+delta, y+height/2, width-delta, y+height/2));
+            case 3:outlList.add(new Rectangle(x+delta, y+height/2, x+width-delta, y+height/2));
                 break;
-            case 4:outlList.add(new Rectangle(width, y+(height/2)+delta, width, y+height-delta));
+            case 4:outlList.add(new Rectangle(x+width, y+(height/2)+delta, x+width, y+height-delta));
                 break;
-            case 5:outlList.add(new Rectangle(x+delta, y+height, width-delta, y+height));
+            case 5:outlList.add(new Rectangle(x+delta, y+height, x+width-delta, y+height));
                 break;
             case 6:outlList.add(new Rectangle(x, y+(height/2)+delta, x, y+height-delta));
                 break;
@@ -60,10 +80,10 @@ public class MyPanel extends JPanel {
         super.paintComponent(g);
 
         // Draw Text
-        //g.drawString("this is my custom Panel!", 10,20);
+        //g.drawString("This is my custom Panel!", 10,20);
         for (Rectangle rect : outlList) {
-        g.drawLine(x,y,width,height);
+             g.drawLine(x,y,width,height);
         }
     }
 
-}
+} 
